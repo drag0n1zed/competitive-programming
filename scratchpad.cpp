de39272c5c0c5178c557943d5622cb28fb6013ca
drag0n1zed <62168687+drag0n1zed@users.noreply.github.com>
2025-11-29 10:34:05 +0800

    Environment setup

---
+++ scratchpad.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+
+void solve() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifdef LOCAL
+    freopen(PROJECT_ROOT "/input.txt", "r", stdin);
+    // freopen("../output.txt", "w", stdout); // Output to file
+#endif
+    int t = 1;
+    // cin >> t; // Multiple test cases
+    while (t--) { solve(); }
+    return 0;
+}
